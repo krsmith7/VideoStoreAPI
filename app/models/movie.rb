2a87d4a4581c6cea3097b0d46d4caee3006e9002
Katricia Smith <smithkatricia@gmail.com>
2018-11-05 14:23:12 -0800

    Add validations to movie, customer, and rental models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :inventory, presence: true, numericality: {only_integer: true, greater_than: 0}
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,4 @@
 class Rental < ApplicationRecord
+  validates :checkout, presence: true
+  validates :due_date, presence: true
 end
