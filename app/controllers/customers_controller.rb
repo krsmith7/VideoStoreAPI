4be2aca860fdc92565cbf8a9b2c7d72878a53bc7
Katricia Smith <smithkatricia@gmail.com>
2018-11-06 14:39:29 -0800

    add filter to fields to customers#index method for json response

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
 
   def index
     customers = Customer.all
-    render json: customers
+    render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone])
   end
 
 end
