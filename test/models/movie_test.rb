0c1de660bcd5d9e1b62be2c7978a2192114c1223
Katricia Smith <smithkatricia@gmail.com>
2018-11-05 16:51:25 -0800

    Add tests for Movie model validations using yml fixtures

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,10 +1,60 @@
 require "test_helper"
 
 describe Movie do
-  # let(:movie) { Movie.new }
+  describe 'validations' do
+    it "must be valid with all params present" do
+      movie = movies(:movie_with_all_params)
+      result = movie.valid?
 
-  it "must be valid" do
-    movie = movies(:movie_with_all_params)
-    movie.must_be :valid?
+      expect(result).must_equal true
+    end
+
+    it "must be valid with only title and inventory present" do
+      movie = movies(:movie_with_title_and_inv)
+      result = movie.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "must have a unique title" do
+      movie = Movie.new(title: "Parkor", inventory: 7)
+      movie.title = movies(:existing_movie).title
+
+      result = movie.valid?
+      expect(result).must_equal false
+    end
+
+    it "must be invalid without a title" do
+      movie = movies(:existing_movie)
+      movie.title = nil
+
+      result = movie.valid?
+      expect(result).must_equal false
+    end
+
+    it "must be invalid without inventory" do
+      movie = movies(:existing_movie)
+      movie.inventory = nil
+
+      result = movie.valid?
+      expect(result).must_equal false
+    end
+
+    it "must be invalid if inventory is not an integer" do
+      movie = movies(:movie_with_all_params)
+      movie.inventory = "three"
+
+      result = movie.valid?
+      expect(result).must_equal false
+    end
+
+
+    it "must be invalid if inventory is less than 1" do
+      movie = movies(:movie_with_all_params)
+      movie.inventory = 0
+
+      result = movie.valid?
+      expect(result).must_equal false
+    end
   end
 end
