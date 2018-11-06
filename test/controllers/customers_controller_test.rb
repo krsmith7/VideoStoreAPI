c04240bea6e68861334b234f76eca0035e5defa9
Katricia Smith <smithkatricia@gmail.com>
2018-11-06 14:29:58 -0800

    Add tests for customers#index

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe 'index' do
+    it "is a functioning route" do
+      get customers_path
+      must_respond_with :success
+    end
+
+    it "returns response with json content" do
+      get customers_path
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "returns an Array from parsed JSON data" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns correct number of Customers" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Customer.count
+    end
+  end
 end
