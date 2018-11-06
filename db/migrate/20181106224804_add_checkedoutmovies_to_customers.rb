e346eaf9700af23bd13dbc8e436342210c4de6d2
Katricia Smith <smithkatricia@gmail.com>
2018-11-06 14:53:39 -0800

    Migration for movies_checked_out column in customers db

--- db/migrate/20181106224804_add_checkedoutmovies_to_customers.rb
+++ db/migrate/20181106224804_add_checkedoutmovies_to_customers.rb
@@ -1,5 +1,5 @@
 class AddCheckedoutmoviesToCustomers < ActiveRecord::Migration[5.2]
   def change
-    add_column :customers, :movies_checked_out_count, default: 0
+    add_column :customers, :movies_checked_out_count, :integer, default: 0
   end
 end
