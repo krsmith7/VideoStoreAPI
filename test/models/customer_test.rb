b2d20539204b0febd2b720dc14c8cab99825d621
Hayden Walls <hwilliams.deva@gmail.com>
2018-11-06 11:48:57 -0800

    customers model test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,37 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe 'validations' do
+
+    before do
+      @customer = customers(:jarth)
+    end
+
+    it 'is valid when all fields are pwesent' do
+      result = @customer.valid?
+
+      expect(result).must_equal true
+    end
+
+    it 'fails without name arg' do
+      @customer.name = nil
+
+      result = @customer.valid?
+
+      expect(result).must_equal false
+      expect(@customer.errors.messages).must_include :name
+    end
+
+    it 'fails when name is empty str' do
+      @customer.name = ""
+
+      result = @customer.valid?
+
+      expect(result).must_equal false
+      expect(@customer.errors.messages).must_include :name
+    end
+
   end
+
 end
