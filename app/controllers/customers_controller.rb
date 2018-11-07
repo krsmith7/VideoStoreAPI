aafa41ca8fb0a0c205c61312c3bf3bda47905e50
Katricia Smith <smithkatricia@gmail.com>
2018-11-07 14:48:32 -0800

    Add case statement for sorting customers#index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,32 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all
+    if params[:sort].present?
+      sort_field = params[:sort]
+
+      case sort_field
+        when "name"
+          customers = Customer.all.sort_by do |customer|
+            customer[:name]
+          end
+        when "id"
+          customers = Customer.all.sort_by do |customer|
+            customer[:id]
+          end
+        when "postal_code"
+          customers = Customer.all.sort_by do |customer|
+            customer[:postal_code]
+          end
+        when "registered_at"
+          customers = Customer.all.sort_by do |customer|
+            customer[:registered_at]
+          end
+      end
+
+    else
+      customers = Customer.all
+    end
+
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count])
   end
 
