58534df6a8d62a915becf8af74560da0afa4b893
K. R. Smith <smithkatricia@gmail.com>
2018-11-07 12:14:38 -0800

    Movie customer contr tests (#2)
    * Delete binding.pry from customer model test
    
    * Delete movie model validation test for inventory > 0
    
    * Add validation to movie model for inventory numericality only integers
    
    * Adds tests for movies#show
    
    * Adds additional movies fixture
    
    * Adds movies#create tests
    
    * Adds assertion to movies#show not found test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :inventory, presence: true
+  validates :inventory, presence: true, numericality: { only_integer: true }
 
   has_many :rentals
 
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -37,4 +37,61 @@ describe MoviesController do
       end
     end
   end
+
+  describe "show" do
+    it "can get one movie" do
+      get movie_path(movies(:movie).id)
+
+      body = JSON.parse(response.body)
+      must_respond_with :success
+      expect(body).must_be_kind_of Hash
+    end
+
+    it "responds with not found error message if no movie is found" do
+      invalid_id = 00
+      get movie_path(invalid_id)
+      must_respond_with :not_found
+
+      body = JSON.parse(response.body)
+      expect(body["errors"]).must_include "movie_id"
+    end
+  end
+
+  describe "create" do
+    let(:movie_input) {
+      {
+        title: "Gingerbread Ginger",
+        overview: "A tale of two gingerbread people",
+        release_date: "1998-12-25",
+        inventory: 5
+      }
+    }
+
+    it "creates new movie given valid data" do
+      expect {post movies_path, params: movie_input
+        }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      created_movie = Movie.find(body["id"].to_i)
+      expect(created_movie.title).must_equal movie_input[:title]
+      must_respond_with :success
+    end
+
+    it "returns bad request error for missing title" do
+      movie_input["title"] = nil
+
+      expect {
+        post movies_path, params: movie_input
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+    end
+  end
+
 end
