2685c06225fd97cd77131afd996f5257097eb9b6
Katricia Smith <smithkatricia@gmail.com>
2018-11-08 09:52:36 -0800

    Refactor customers#index sorting

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,28 +1,9 @@
 class CustomersController < ApplicationController
 
   def index
-    if params[:sort].present?
+    if params[:sort]
       sort_field = params[:sort]
-      # REFACTOR: use compact if [array] statement
-      case sort_field
-        when "name"
-          customers = Customer.all.sort_by do |customer|
-            customer[:name]
-          end
-        when "id"
-          customers = Customer.all.sort_by do |customer|
-            customer[:id]
-          end
-        when "postal_code"
-          customers = Customer.all.sort_by do |customer|
-            customer[:postal_code]
-          end
-        when "registered_at"
-          customers = Customer.all.sort_by do |customer|
-            customer[:registered_at]
-          end
-      end
-
+      customers = Customer.all.order(sort_field)
     else
       customers = Customer.all
     end
